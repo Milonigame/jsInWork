2189c19e8a1e82190dbcbf1293c77e8d19eada2b
Mariia <milonigame1@gmail.com>
2023-02-22 13:37:09 +0300

    second

---
+++ thisNewConsol/constructorThis/constru.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function User(name,id){
+    this.name=name;
+    this.id=id;
+    this.human=true;
+    this.hello=function(){
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit=function(){
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan=new User('Ivan', 28);
+const alex=new User('Alex', 20);
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
+
+//hjgh
+
+
+
---
+++ thisNewConsol/constructorThis/this.js
@@ -0,0 +1,52 @@
+//'use strict'
+// function showThis(a,b){
+//     console.log(this);
+//    function sum(){
+//     console.log(this);
+//     return this.a + this.b;
+//    } 
+//    console.log(sum());
+// }
+// showThis(4,5);
+
+// const obj={
+// a:20,
+// b:15,
+// sum:function(){
+//     function shout(){
+//     console.log(this);
+// }
+// shout();
+// }
+// };
+// obj.sum();
+
+function sayName(surname){
+    console.log(this);
+    console.log(this.name+surname);
+}
+const user={
+    name:'John'
+};
+sayName.call(user, ' Smith');//Ручное присвоение контекста
+sayName.apply(user, [' Smith']);//Ручное присвоение контекста
+
+function count(num){
+    return this*num;
+}
+
+const double=count.bind(2);//double-это новая функция у 
+//которой жестко привязанный контекст 2(которая будет удваивать num),
+//который передается в this
+console.log(double(3));
+console.log(double(13));
+
+
+//1)Обычная функцияЖ this=window, но если use strict undefined
+//2)Контекст у методов объекта-сам объект
+//3)this в конструкторах и классах-это новый экземпляр объекта
+//4)Ручная привязка this: call,apply,bind
+
+
+//hbujujgvhuj
+
